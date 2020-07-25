84e05d8438e116a0f0214d608ee4a7d93da5f81c
geekxiong <geekxiong@foxmail.com>
2020-07-25 17:18:39 +0800

    新增页面以及相关的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ProblemDetail from '@/pages/ProblemDetail'
-import Index from '@/pages/Index'
 
-Vue.use(Router)
+import Index from '@/pages/Index'
+import ProblemList from '@/pages/ProblemList'
+import ProblemDetail from '@/pages/ProblemDetail'
+import Status from '@/pages/Status'
+import UserList from '@/pages/UserList'
+import UserDetail from '@/pages/UserDetail'
+
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -14,9 +19,30 @@ export default new Router({
       component: Index
     },
     {
+      path: '/problems',
+      name: 'ProblemList',
+      component: ProblemList
+    },
+    {
       path: '/problem/:ojName-:probId',
       name: 'ProblemDetail',
       component: ProblemDetail
-    }
+    },
+    {
+      path: '/status',
+      name: 'Status',
+      component: Status
+    },
+    {
+      path: '/users',
+      name: 'UserList',
+      component: UserList
+    },
+    {
+      path: '/user/:userId',
+      name: 'UserDetail',
+      component: UserDetail
+    },
+
   ]
 })
